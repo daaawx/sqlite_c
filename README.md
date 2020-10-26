d137e690c5ff24b7cba64f8eb9a6517771aa58d0
Dax <daaawx@gmail.com>
2020-10-26 21:55:50 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Sqlite C
+
+A sqlite clone written entirely in C.
+
+This was built using the following source as a reference:
+
+https://cstack.github.io/db_tutorial/
---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+
+int main(int argc, char* argv[])
+{
+	InputBuffer input_buffer = create_input_buffer();
+	while (true)
+	{
+		print_promt();
+		read_input(input_buffer);
+
+		if (strcomp(input_buffer->buffer, '.exit') == 0)
+		{
+			close_input_buffer(input_buffer);
+			exit(EXIT_SUCCESS);
+		}
+		else
+		{
+			printf("Unrecognized command: '%s'\n", input_buffer->buffer);
+		}
+	}
+}
